4bc16104f6b031da0d3bc003d1cd1c26ba1b4a09
Team Lotus <104599580+Tm-Lotus@users.noreply.github.com>
2024-04-07 10:56:17 +0200

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/lotusabdo/Lotus.git",
+    "https://github.com/Tm-Lotus/CJ.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(

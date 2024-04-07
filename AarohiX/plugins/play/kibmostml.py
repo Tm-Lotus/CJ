1ba0831364a0460f7ce8ab2527898683cb1285f0
Team Lotus <104599580+Tm-Lotus@users.noreply.github.com>
2024-04-07 11:48:49 +0200

    Update kibmostml.py

--- AarohiX/plugins/play/kibmostml.py
+++ AarohiX/plugins/play/kibmostml.py
@@ -85,3 +85,17 @@ async def almortagel(client: Client, message: Message):
     url = f"https://t.me/yoipopl/{rl}"
     await client.send_audio(message.chat.id,url,caption="💚 ¦ تـم اختيـار استوري لـك")
 
+@app.on_message(filters.command(["غنيلي", "غني", "غ", "🎙 ¦ غـنيـلي"], ""))
+async def ihd(client: Client, message: Message):
+    rl = random.randint(3,267)
+    url = f"https://t.me/bsmaatt/{rl}"
+    await client.send_voice(message.chat.id,url,caption="🔥 ¦ تـم اختيـار الاغـنـية لـك",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+                           )
